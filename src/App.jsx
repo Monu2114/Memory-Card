1bbbe5827682f50d3d01559abe1f9d5eebeb2096
Monu_2114 <monu.nanabala@gmail.com>
2024-07-17 19:56:22 +0530

    avoid unnecesary api calls (Shuffling data)

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ function App() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [cardsClicked, setCardsClicked] = useState([]);
+  const [roundCompleted, setRoundCompleted] = useState(false);
 
   const handleClick = (card) => {
     if (cardsClicked.includes(card)) {
       setScore(0);
       setCardsClicked([]);
+      setRoundCompleted(false);
     } else {
       setScore((score) => {
         const newScore = score + 1; //score doesnt get updated immediately
@@ -20,21 +22,26 @@ function App() {
 
         setHighScore((highScore) => Math.max(highScore, newScore));
         return newScore;
-      }); // Use functional update to ensure correct score update
+      });
+      setPokemon(shuffle(pokemon));
+      // Use functional update to ensure correct score update
       setCardsClicked([...cardsClicked, card]);
+      if (cardsClicked.length == 4) {
+        setRoundCompleted(true);
+      }
     }
-
     // Use functional update to ensure highScore reflects the latest score
   };
 
   const [isLoading, setIsLoading] = useState(true); // State for loading indicator
 
   useEffect(() => {
-    const randomOffset = Math.floor(Math.random() * (1302 - 8)); // Generate a random offset
-
     async function fetchData() {
       try {
+        console.log("Fetching data...");
         // Fetch initial list of Pokémon with a limit of 8
+        const randomOffset = Math.floor(Math.random() * (1302 - 8)); // Generate a random offset
+
         const response = await fetch(
           `https://pokeapi.co/api/v2/pokemon?offset=${randomOffset}&limit=8`
         );
@@ -62,8 +69,27 @@ function App() {
     }
 
     fetchData();
-  }, [cardsClicked]);
+  }, [roundCompleted]);
 
+  const shuffle = (array) => {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle
+    while (currentIndex !== 0) {
+      // Pick a remaining element
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // Swap it with the current element
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  };
   return (
     <>
       <div className="header">
